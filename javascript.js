aa61491874f47da1f176f17493fc9baf1e4029c6
Tanne <quachtienthanh92@gmail.com>
2023-04-20 11:50:42 +0200

    update new theme toast

--- javascript.js
+++ javascript.js
@@ -1,46 +1,34 @@
-function toast({ title = "", message = "", type = "", duration = 3000 }) {
+function toast({ title = "", message = "", type = "info", duration = 3000 }) {
     const main = document.getElementById("toast");
     if (main) {
         const toast = document.createElement("div");
-        toast.classList.add("toast", `toast--${type}`);
-        const delay = (duration / 1000).toFixed(2);
-        toast.style.animation = `slideInLeft ease 0.5s, fadeOut linear 1s ${delay}s  forwards`;
         const icons = {
-            success: "fas fa-check-circle",
-            info: "fas fa-info-circle",
-            warning: "fas fa-exclamation-circle",
-            error: "fas fa-exclamation-circle",
+            success: "fa-solid fa-circle-check",
+            error: "fa-solid fa-circle-exclamation",
         };
-        toast.onclick = function (e) {
-            if (e.target.closest(".toast__close")) {
-                main.removeChild(toast);
-                clearTimeout(removeToastAuto);
-            }
-        };
+        const icon = icons[type];
+        toast.classList.add("toast", `toast__${type}`);
+        const delay = (duration / 1000).toFixed(2);
+
         const removeToastAuto = setTimeout(function () {
             main.removeChild(toast);
         }, duration + 1000);
-        const icon = icons[type];
         toast.innerHTML = `
-        <div class="toast__icon"> 
-        <i class="${icon}"></i>
+        <i class="${icon} toast__icon"></i>
+        <div class="toast__body">
+        <h3 class="toast__title"> ${title}</h3>
+        <p class="toast_message"> ${message}</p>
         </div>
-        <div class="toast__title">
-        <h3 class="toast__title" >
-                ${title}!</h3>
-            <p class="toast__msg" >${message}.</p>
+        <i class="fas fa-times toast__close"></i>  
         </div>
-        <div class="toast__close">
-        <i class="fas fa-times"></i>    
         </div>`;
         main.appendChild(toast);
     }
 }
-
 function showSuccessToast() {
     toast({
         title: "Success",
-        message: "A success Toast ",
+        message: "You can access all the files in this folder",
         type: "success",
         duration: 3000,
     });
@@ -48,7 +36,7 @@ function showSuccessToast() {
 function showErrorToast() {
     toast({
         title: "Error",
-        message: "A error toast.",
+        message: "You can't access all the files ",
         type: "error",
         duration: 3000,
     });
